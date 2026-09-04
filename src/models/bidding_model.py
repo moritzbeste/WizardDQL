ca77cea02548ab62a12d92b332457abfb08618a5
Moritz Beste <159854683+moritzbeste@users.noreply.github.com>
2026-09-04 20:20:14 +0200

    basic models implemented

--- src/models/bidding_model.py
+++ src/models/bidding_model.py
@@ -1,5 +1,20 @@
 import torch
 
 class BiddingModel(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size, output_size):
         super().__init__()
+
+        sizes = [input_size, input_size * 2, input_size, output_size]
+        layers = []
+        for in_size, out_size in zip(sizes[:-1], sizes[1:]):
+            layers.extend([
+                nn.Linear(in_size, out_size),
+                nn.BatchNorm1d(out_size),
+                nn.ReLU(),
+            ])
+        
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.model(x)
+        
